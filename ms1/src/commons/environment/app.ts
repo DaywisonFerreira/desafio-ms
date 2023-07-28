6fee0ede29c709ab2fb3ebea45c45d9419db12db
Daywison Leal <daywison@MacBook-Pro-de-Daywison.local>
2023-07-28 12:39:41 -0300

    chore: create commons env

---
+++ ms1/src/commons/environment/app.ts
@@ -0,0 +1,4 @@
+export const NODE_ENV: string = process.env.NODE_ENV || 'local';
+export const APPLICATION_NAME: string = process.env.APPLICATION_NAME;
+export const APPLICATION_VERSION: string = process.env.APPLICATION_VERSION;
+export const APPLICATION_PORT: string = process.env.APPLICATION_PORT || '3000';
