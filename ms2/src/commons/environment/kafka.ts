ba865d46782c52339ccbdd24ef9f6c03740c0230
Daywison Leal <daywison@MacBook-Pro-de-Daywison.local>
2023-07-28 14:48:48 -0300

    chore: create commons env

---
+++ ms2/src/commons/environment/kafka.ts
@@ -0,0 +1,5 @@
+export const KAFKA_CLIENT_ID: string = process.env.KAFKA_CLIENT_ID;
+export const KAFKA_URIS: string = process.env.KAFKA_URIS;
+export const KAFKA_GROUP_ID: string = process.env.KAFKA_GROUP_ID;
+export const KAFKA_TOPIC_CREATE_NEW_USER: string =
+  process.env.KAFKA_TOPIC_CREATE_NEW_USER;
