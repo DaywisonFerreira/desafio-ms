db6c1e0023b673dbf7fe8c13d87c29556b0ac6aa
Daywison Leal <daywison@MacBook-Pro-de-Daywison.local>
2023-07-29 15:53:33 -0300

    chore: create dockerfile and docker compose

--- ms1/src/presentation/controllers/users.controller.ts
+++ ms1/src/presentation/controllers/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Logger, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { SendUserService } from 'src/application/usecases/users/send-user.service';
@@ -11,12 +11,13 @@ export class UsersController {
 
   @Post()
   @ApiOperation({ summary: 'Send an user to be created' })
+  @ApiResponse({ status: 200 })
   @ApiResponse({ status: 400, description: 'Bad Request' })
-  async send(@Body() input: CreateUserDto): Promise<string> {
+  @HttpCode(200)
+  async send(@Body() input: CreateUserDto): Promise<void> {
     try {
-      this.logger.log(`Receive a new user: ${input.email}`);
+      this.logger.log(`Receive a new user with e-mail: ${input.email}`);
       await this.sendUserService.execute(input);
-      return 'User will be created';
     } catch (error) {
       this.logger.error(error);
       throw error;
